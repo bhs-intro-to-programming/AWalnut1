8c4c4314e4c5289429016837f3b35a25862106bf
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-08-18 15:14:02 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -51,6 +51,6 @@ drawFilledRect(150, 200, 200, 200, 'red')
 
 drawFilledRect(250, 230, 100, 50, 'white')
 
-drawFilledRect(180, 400, 30, 70, 'red')
+drawFilledRect(180, 400, 50, 70, 'red')
 
-drawFilledRect(270, 400, 30, 70, 'red')
+drawFilledRect(270, 400, 50, 70, 'red')
