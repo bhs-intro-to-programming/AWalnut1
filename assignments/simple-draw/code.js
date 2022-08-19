fa6ae6bb624ab7b753ab63a01e211af81abf6218
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-08-19 14:54:08 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -57,4 +57,4 @@ drawFilledRect(270, 400, 50, 70, 'red')
 //amogus leg right
 drawFilledRect(80, 250, 70, 100, 'red')
 //amogus backpack
-draw(130, 182, 40, 'green')
+drawFilledTri(130, 182, 40, 'green')
