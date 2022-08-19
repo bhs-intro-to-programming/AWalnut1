70100e430459ac8a18d2b84b2471811ae3ff925d
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-08-19 15:21:42 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -41,8 +41,6 @@ drawCircle(width/69, height/69, 69, 'blue');
 
 drawRect(69, 69, 69, 69, 'black');
 
-drawFilledCircle(69, height - 69, 69, 'red)
-
 drawFilledRect(69, 68, 69, 69, '#cd3024');
 
 drawLine(30, 100, width, height, 'blue')
