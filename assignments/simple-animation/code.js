3099aeeea72648b5e123500f4bfc0a711d1bb760
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-08-25 15:25:21 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -6,7 +6,7 @@
 // number of milliseconds (one millionth of a second) since the program started.
 const drawFrame = (time) => {
   clear();
-  drawFilledCircle((time / 100) % width, height / 4, 100, 'blue');
+  drawFilledCircle((time / 50) % width, height / 4, 100, 'blue');
   drawFallingTriangle(width / 2, time);
 };
 
