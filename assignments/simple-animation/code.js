e422d294d2808f1394f3964f9b79288cb919d5ed
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-09-01 15:11:40 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -19,12 +19,7 @@ const drawTriangle = (x1, y1, x2, y2, x3, y3, color, width = 1) => {
   drawLine(x3, y3, x1, y1, color, width);
 };
 
-const drawSquare = (x1, y1, x2, y2, x3, y3, x4, y4, color, width = 1) => {
-  drawLine (x1, y1, x2, y2, color, width);
-  drawLine (x2, y2, x3, y3, color, width);
-  drawLine (x3, y3, x4, y4, color, width);
-  drawLine (x4, y4, x1, y1, color, width);
-}
+
 
 // This draws a falling trangle of a particular shape whose bottom point is
 // positioned at x and whose y is a function of time.
@@ -43,7 +38,7 @@ const drawFallingTriangle = (x, time) => {
   drawTriangle(x1, y1, x2, y2, x3, y3, 'blue', 10);
 };
 
-drawSquare(30, 29, 29, 18, 29, 10, 58, 59, 'red', 10);
+drawRect(40, 19, 49 ,2, 'red')
 
 
 // Leave this code here or the animation won't run. Also don't change the name
