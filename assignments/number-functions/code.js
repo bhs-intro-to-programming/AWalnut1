a87d15e3fc1def31384d9e2662d44ee46c746c79
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-09-07 15:10:38 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -6,6 +6,10 @@ function subtract (a,b) {
    return a-b
 }
 
+function multiply (a,b) {
+  return a*b
+}
+
 function manhattanDistance (a1, b1, a2, b2) {
   return (Math.abs(a1-a2))+(Math.abs(b1-b2))
 }
