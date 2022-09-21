77b74f773005eb48f5fba64ce7e3b8d1f3e4d67b
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-09-21 15:20:23 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -2,13 +2,13 @@
 //
 // If s is a string:
 //
-//  s.search(/[aeiou]/) returns the index in s of the first vowel.
+// s.search(/[aeiou]/) returns the index in s of the first vowel.
 //
 // For instance
 //
-//  const s = 'phone';
+// const s = 'phone';
 //
-//  s.search(/[aeiou]/) returns 2
+// s.search(/[aeiou]/) returns 2
 //
 // The funny syntax /[aeiou]/ is called a "regular expression". We'll learn
 // about them in more detail later.
@@ -19,13 +19,13 @@
 //
 // It looks like:
 //
-//  booleanValue ? value1 : value2
+// booleanValue ? value1 : value2
 //
 // And it evaluates to value1 is booleanValue is true and value2 if it is false.
 //
 // For example:
 //
-//  x === 0 ? "Zero" : "Not zero"
+// x === 0 ? "Zero" : "Not zero"
 //
 // will evaluate to either "Zero" or "Not zero" depending on whether x === 0.
 //
@@ -42,7 +42,6 @@ const pigLatin = (s) => {
 const advancedPigLatin = (s) => {
   let vowels = /[aeiou]/
   let a = s.search(vowels);
-  return a === 0? "Zero" : "Not zero" 
+  return a = 0? "Zero" : "Not zero" 
 }
 
-const a = 'help'
