ac8afd72b13bff740d5777ad844ab6769a43c3d2
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-09-22 15:15:10 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -42,8 +42,8 @@ const advancedPigLatin = (s) => {
   let vowels = /[aeiou]/
   let a = s.search(vowels);
   if (a === 0) {
-    return s.substring+'way'
+    return s.substring()+'way'
   } else {
-    return s.substring(s.length, n) + s.substring(0, n) + 'ay';
+    return s.substring(s.length, a) + s.substring(0, a) + 'ay';
   }
 }
