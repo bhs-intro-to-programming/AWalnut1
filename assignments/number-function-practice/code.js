f60dfe78313bf8c62447b5164cd03b90931d2cd4
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-10-05 15:31:50 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -1,22 +1,21 @@
 const add = (a, b) => {
-  return a+b
+  return a + b
 } // starting small
 
-const averageOfThree = (a,b,c) => {
-  return (a+b+c) / 3
+const averageOfThree = (a, b, c) => {
+  return (a + b + c) / 3
 }; // getting better
 
 // i just spent the last 15 mins trying to do a cool thing with like let loops and shit and then realised its the exact same as
 
-const multiply = (a,b) => {
-  return a*b
+const multiply = (a, b) => {
+  return a * b
 } // it would have been cool tho, trust me
 
-const addButThenAddOneMore = (a,b) => {
-  return (a+b)+1
-}; // i thought it would be funny
+const addButThenAddOneMore = (a, b) => {
+  return (a + b) + 1
+}; // i thought it would be funny, could be shortened to just addOneMore
 
-const nothing = (a,b) => {
-  return a
-  return b
-};
+const nothing = (a, b) => {
+  return (a),(b)
+}; // i dont understand why a function that takes two variables and returns thos two variables unchanged is so dang hard to program but it is
