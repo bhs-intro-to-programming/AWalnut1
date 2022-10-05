c4655885bea62d6c05bf3776e30bf819c4bbf99e
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-10-05 15:11:23 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -7,6 +7,7 @@ const averageOfThree = (a,b,c) => {
 }; // getting better
 
 // i just spent the last 15 mins trying to do a cool thing with like let loops and shit and then realised its the exact same as
+
 const multiply = (a,b) => {
   return a*b
 }
