7da5a401b04935f3aefbf3949dc956e37491c131
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-10-07 14:58:02 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -7,3 +7,31 @@ const G = 6.6743e-11;
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+
+const itemsLeftOver = (numberOfPeople, numberOfItems) => {
+  while (numberOfPeople > numberOfItems) {
+    numberOfPeople - numberOfItems
+    console.log(numberOfItems)
+  }
+  return
+};
+
+const areaOfCircle = (radius) => {
+  return Math.PI * radius ** 2
+}; // didnt see the proper syntax in the question for a second there
+
+const volumeOfCube = (edge) => {
+  return
+};
+
+const populationGrowth = (population, growth) => {
+  return population * (1 + growth)
+};
+
+const earnedRunAverage = (earnedRuns, innings) => {
+  return ((earnedRuns + innings) / 2) * 9
+};
+
+const valueOfJewels = (numDiamonds, numEmeralds, valueDiamonds, valueEmeralds) => {
+  return (numDiamonds * valueDiamonds) + (numEmeralds, valueEmeralds)
+};
