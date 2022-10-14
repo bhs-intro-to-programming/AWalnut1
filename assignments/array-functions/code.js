8c8ec3f28649d9288dc14e69553b0634a12115bd
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-10-14 14:43:02 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,7 @@
 const countTens = (xs) => {
   let tens = 0
-  for (let xs = [0]; xs < xs.length; xs++) {
-    if (xs [10]) {
+  for (let i = [0]; i < i.length; i++) {
+    if (i [10]) {
       tens+1
     }
   }
