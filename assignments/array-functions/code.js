b601c0a1cd9ca0ba250d7444b8f6f00dfe83b904
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-10-19 14:42:22 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -6,7 +6,4 @@ const countTens = (xs) => {
     }
   }
   return tens;
-} // damn i really did just stare out the window for half an hour fuck me
-// i feel as if i am so incredibly close but just missing like one or two itty bitty detailes to make it all come together
-// i know i cant have the let be xs as its already consted but when i make the let something else xs is never read in the actual function, making it useless
-// idk man shits fucked
+} 
