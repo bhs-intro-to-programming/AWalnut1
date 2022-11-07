bb4727abe5a1377db10d875c03a02e23c06167aa
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-07 16:14:11 -0700

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -2,12 +2,16 @@ const getX = (object) => {
   return object.x
 };
 
-const point = (x,y) => {
-  let object = {x, y}
+const point = (x, y) => {
+  let object = { x, y }
   return object
 };
 
 const emptyObject = () => {
   let object = {}
   return object
+};
+
+const distance = (obj1, obj2) => {
+  return
 };
