86b32c885d3567e5e49161ded2b57b90626fbb71
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-08 15:45:28 -0700

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -17,8 +17,8 @@ const distance = (p1, p2) => {
 }; // i saw your thing but the math.abs's seem to make it work, idk why
 
 const midpoint = (p1,p2) => {
-  let object = {x,y}
   let x = ((p1.x + p2.x) / 2)
   let y = ((p1.y + p2.y) / 2)
+  let object = {x,y}
   return object
 };
