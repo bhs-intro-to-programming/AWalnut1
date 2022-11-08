68a57b1d07bc5e1acf22841f99d4cc02610af6ad
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-08 15:37:20 -0700

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -13,5 +13,5 @@ const emptyObject = () => {
 }; //you could probably also just do return {} but also boring
 
 const distance = (obj1, obj2) => {
-  return Math.sqrt((obj1.x - obj2.x)**2) + (obj1.y - obj2.y)**2)
+  return Math.sqrt((obj1.x - obj2.x)**2) + (obj1.y - obj2.y)**2
 };
