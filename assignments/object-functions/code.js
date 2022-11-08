1ff4948db84296f02d0a8152eb09f98bb9d8e5fc
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-08 15:40:59 -0700

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -12,6 +12,10 @@ const emptyObject = () => {
   return object
 }; //you could probably also just do return {} but also boring
 
-const distance = (obj1, obj2) => {
-  return (Math.sqrt((Math.abs(obj1.x - obj2.x)**2)+(Math.abs(obj1.y - obj2.y)**2)))
+const distance = (p1, p2) => {
+  return (Math.sqrt((Math.abs(p1.x - p2.x)**2)+(Math.abs(p1.y - p2.y)**2)))
+}; // i saw your thing but the math.abs's seem to make it work, idk why
+
+const midpoint = (p1,p2) => {
+  return
 };
