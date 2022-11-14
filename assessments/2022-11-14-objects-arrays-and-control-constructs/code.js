a2f24976c33515da93a449f8b18b23a21c54f779
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-14 15:17:13 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -22,7 +22,7 @@ const isSamePoint = (p1, p2) => {
 };// cant for the life of me remember how to do proper if statments so were moving on
 
 const totalWithTip = (bill, tipPercentage) => {
-  let money = {}
+  let money = {'subtotal' : 0, 'tip' : 0, 'total' : 0}
   let money.subtotal = bill.subtotal
   let money.tip = bill.subtotal * tipPercentage
   let money.total = bill.subtotal * (1+tipPercentage)
