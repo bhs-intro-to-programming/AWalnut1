e734f3e27a5d748f6f021084090e5ab965d5c395
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-15 14:55:00 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,7 @@
 // doing this new on 11/15/22 as my arrays is a 1/4
 
 const countTens = (array) => {
-  let tens = 0
+  let tens = 0;
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 10) {
       tens++
