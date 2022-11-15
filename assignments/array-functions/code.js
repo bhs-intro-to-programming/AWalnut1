7bcebccedc64efd4304ebb75964dcb37055236f6
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-15 14:55:55 -0800

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -14,7 +14,7 @@ const sum = (array) => {
   let numbers = 0;
   for (let i = 0; i < array.length; i++) {
     let numbers = (numbers + array[i])
-    console.log(numbers)
+    console.log(i)
   }
   return numbers
 };
