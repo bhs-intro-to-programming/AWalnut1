1bcc4f92c13c8a7f6fbbd27e801ff6655461d174
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-16 15:19:50 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -9,3 +9,8 @@ const stringContains = (s1, s2) => {
 const firstAndLast = (s) => {
   return s.substring(0, 1) + s.substring(s.length-1)
 };
+
+const swapFrontAndBack = (s) => {
+  let cutOff = Math.floor(s.length / 2)
+  return s.substring(cutOff, s.length-1) + s.substring(0, cutOff)
+};
