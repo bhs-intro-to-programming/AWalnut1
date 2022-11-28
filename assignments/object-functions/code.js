f4ec32880e01556e3f2ab2f8b34311775d6036da
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-28 15:30:50 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -13,27 +13,27 @@ const emptyObject = () => {
 }; //you could probably also just do return {} but also boring
 
 const distance = (p1, p2) => {
-  return Math.sqrt((Math.abs(p1.x - p2.x)**2)+(Math.abs(p1.y - p2.y)**2))
+  return Math.sqrt((Math.abs(p1.x - p2.x) ** 2) + (Math.abs(p1.y - p2.y) ** 2))
 }; // i saw your thing but the math.abs's seem to make it work, idk why
 
-const midpoint = (p1,p2) => {
-  return { x: ((p1.x + p2.x) /2), y: ((p1.y +p2.y) /2)}
+const midpoint = (p1, p2) => {
+  return { x: ((p1.x + p2.x) / 2), y: ((p1.y + p2.y) / 2) }
 }; // figured out :)
 
 const sumSalaries = (salaries) => {
-  let sum=0
-  for(let i=0; i < salaries.length; i++) {
-   sum = sum + salaries[i].salary
+  let sum = 0
+  for (let i = 0; i < salaries.length; i++) {
+    sum = sum + salaries[i].salary
   }
   return sum
 };
 
 const newHighScore = (high, scores) => {
   let newHigh = high
-  for (let i=0; i > scores.length; i++)  {
+  for (let i = 0; i > scores.length; i++) {
     console.log(scores[i].score)
     if (scores[i].score > high) {
-    newHigh = scores[i].score
+      newHigh = scores[i].score
     }
   }
   return newHigh
