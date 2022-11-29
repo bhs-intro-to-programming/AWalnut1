c7d29d4e1542c33c6e44a9d6a27c5123b0c21cc8
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-29 15:08:55 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -1,8 +1,12 @@
-const totalEggs = (hardEggsWanted, softEggsWanted) => { return hardEggsWanted + softEggsWanted };
+const totalEggs = (hardEggsWanted, softEggsWanted) => { hardEggsWanted + softEggsWanted };
 
-const chocolatesPerPerson = (chocolates, numPeople) => { return Math.floor(chocolates / numPeople) };
+const chocolatesPerPerson = (chocolates, numPeople) => {
+  return Math.floor(chocolates / numPeople)
+};
 
-const extraChocolates = (chocolates, numPeople) => { return chocolates % numPeople };
+const extraChocolates = (chocolates, numPeople) => {
+  return chocolates % numPeople
+};
 
 const leftOut = (chocolates, numPeople) => {
   if (numPeople - chocolates > 0) {
@@ -11,16 +15,26 @@ const leftOut = (chocolates, numPeople) => {
   return 0
 }; //probably an easier way to do this
 
-const probabilityAllHeads = (coinFlips) => { return 0.5 ** coinFlips };
+const probabilityAllHeads = (coinFlips) => {
+  return 0.5 ** coinFlips
+};
 
-const futureHour = (currentHour, futureHours) => { return currentHour + (futureHours % 24) };
+const futureHour = (currentHour, futureHours) => {
+  return currentHour + (futureHours % 24)
+};
 
-const presentsBudget = (numFriends, avgPresentPrice) => { return numFriends * avgPresentPrice };
+const presentsBudget = (numFriends, avgPresentPrice) => {
+  return numFriends * avgPresentPrice
+};
 
-const perPresent = (totalMoney, presentsNeeded) => { return totalMoney / presentsNeeded };
-// i remembered this one because i was suprised that "per" was a division term in the lecture the other day
+const perPresent = (totalMoney, presentsNeeded) => {
+  return totalMoney / presentsNeeded
+}; // i remembered this one because i was suprised that "per" was a division term in the lecture the other day
 
-const wrappingCombos = (kindsOfPaper, kindsOfRibbons, kindsOfBows) => { return kindsOfPaper * kindsOfRibbons * kindsOfBows };
+const wrappingCombos = (kindsOfPaper, kindsOfRibbons, kindsOfBows) => {
+  return kindsOfPaper * kindsOfRibbons * kindsOfBows
+};
 
-const biggestNumber = (number) => { return 10 ** number - 1 };
-// thanks for the hint, would have torn my hair out about this one without it
+const biggestNumber = (number) => {
+  return 10 ** number - 1
+}; // thanks for the hint, would have torn my hair out about this one without it
