75998df8e730e2bac79cbec022497d7bccf40e04
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-29 14:33:53 -0800

    Updating assessments/2022-11-29-numeric/code.js

---
+++ assessments/2022-11-29-numeric/code.js
@@ -0,0 +1,7 @@
+const totalEggs = (hardEggsWanted, softEggsWanted) => {
+return hardEggsWanted + softEggsWanted
+};
+
+const chocolatesPerPerson = (chocolates, numPeople) => {
+  return Math.floor(chocolates / numPeople)
+};
