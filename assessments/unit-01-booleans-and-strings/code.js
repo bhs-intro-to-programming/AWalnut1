7c8178d862e2c50eed450c858b561136dcd751df
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-11-30 15:10:01 -0800

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -36,3 +36,7 @@ const isLeapYear = (year) => {
 const firstAndLast = (s) => {
   return s.substring(0,1) + s.substring(s.length - 1)
 };
+
+const swapFrontAndBack = (s) => {
+  return s.substring(s.length/2, s.length-1) + s.substring(0, (s.length/2)-1)
+};
