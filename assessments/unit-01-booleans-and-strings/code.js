dce48dab6a494db9d9c6ff70079f4c717c490128
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-12-01 14:53:34 -0800

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -52,7 +52,7 @@ const randomCharacter = (s) => {
 
 const randomCharacterUpDown = (s) => {
   let randy = rand(s.length)
-  let rander = s.substring(randy) 
+  let rander = s.substring(randy, randy+1) 
   console.log(rander)
   return s.toUpperCase(rander) + s.toLowerCase(rander) + rander
 };
