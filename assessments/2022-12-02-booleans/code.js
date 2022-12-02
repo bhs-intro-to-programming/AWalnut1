3ba1bc3ed7c957c11267eba5229e27a0ea979dd9
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-12-02 14:44:08 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -1,3 +1,7 @@
 const needHeavyCoat = (goingOutside, coldOut) => {
   return goingOutside && coldOut
 };
+
+const needSunscreen = (goingToBeach, goingSkiing) => {
+  return goingToBeach || goingSkiing
+};
