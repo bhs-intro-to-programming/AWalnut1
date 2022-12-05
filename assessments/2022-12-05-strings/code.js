be977778a78f5bfc10a158402466d72b3bc277cf
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-12-05 14:59:10 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -1,10 +1,10 @@
 const upToX = (s) => {
   let x = s.indexOf('x')
   return s.substring(0,x)
-};
+}; // i changed nothing and it worked ok
 
-const charactersAround = () => {
-  return
+const charactersAround = (s, index) => {
+  return s.substring (index-1, index) + s.substring (index+1, index+2)
 }
 
 const middle = () => {
