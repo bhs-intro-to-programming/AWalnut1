5b0be47c013bef59d785413d465e17cfde40a63e
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-12-05 14:52:51 -0800

    Updating assessments/2022-12-05-strings/code.js

---
+++ assessments/2022-12-05-strings/code.js
@@ -0,0 +1,31 @@
+const upToX = (s) => {
+  return s.substring(0,s.indexof('x'))
+}
+
+const charactersAround = () => {
+  return
+}
+
+const middle = () => {
+  return
+}
+
+const pair = () => {
+  return
+}
+
+const containsX = () => {
+  return
+}
+
+const slug = () => {
+  return
+}
+
+const capitalize = () => {
+  return
+}
+
+const capitalizeName = () => {
+  return
+}
