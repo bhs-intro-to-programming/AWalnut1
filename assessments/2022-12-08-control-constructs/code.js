6e084d5f5f87748bb4d39892efece3659e5f8757
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-12-08 15:22:11 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -28,8 +28,12 @@ const containsX = (s) => {
     }
     return false
   }
-};
+};// THIS SHOULD BE LOOPING WHY ISNT ITAAAAAAAA
 
-const sumSquares = () => {
-  return
+const sumSquares = (n) => {
+  let sum = 0
+  for (let i=0; i < n; i++) {
+    sum + i**2
+  }
+  return sum
 };
