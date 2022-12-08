a6124b024125f635ff993a86e59ce5d6beafd4b7
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-12-08 15:00:55 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

---
+++ assessments/2022-12-08-control-constructs/code.js
@@ -0,0 +1,35 @@
+const emit = (a, b) => console.log(a + ', ' + b);
+
+const yesIfEven = (n) => {
+  if (n % 2 === 0) {
+    return 'yes'
+  } return 'no'
+};
+
+const countXs = (s) => {
+  let xs = 0
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'x') {
+      xs++
+    }
+  }
+  return xs
+};
+
+const timesTable = (n) => {
+  return
+};
+
+const containsX = (s) => {
+  for (let i = 0; i < s.length; i++) {
+    console.log(i)
+    if (s[i] === 'x') {
+      return true
+    }
+    return false
+  }
+};
+
+const sumSquares = () => {
+  return
+};
