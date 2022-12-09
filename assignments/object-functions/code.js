3beb3997cb15652e84b30857d6fc1b91e0f28edc
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-12-09 14:56:36 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -3,14 +3,12 @@ const getX = (object) => {
 };
 
 const point = (x, y) => {
-  let object = { x, y }
-  return object
-}; // you could also just do return {x,y} but thats boring
+  return  { x, y }
+}; 
 
 const emptyObject = () => {
-  let object = {}
-  return object
-}; //you could probably also just do return {} but also boring
+  return {}
+};
 
 const distance = (p1, p2) => {
   return Math.sqrt((Math.abs(p1.x - p2.x) ** 2) + (Math.abs(p1.y - p2.y) ** 2))
