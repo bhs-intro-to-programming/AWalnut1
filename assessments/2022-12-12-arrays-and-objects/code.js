85f9a8ed649dd370988c1a4ffbd4f41bebf8deae
AWalnut1 <111320584+AWalnut1@users.noreply.github.com>
2022-12-12 14:53:04 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

---
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -0,0 +1,35 @@
+const makeRow = () => {
+  return ['', '', '']
+};
+
+const makeBoard = () => {
+  return
+};
+
+const makeMove = () => {
+  return
+};
+
+const placeMark = () => {
+  return
+};
+
+const allTheSame = () => {
+  return
+};
+
+const extractColumn = () => {
+  return
+};
+
+const recordMove = () => {
+  return
+};
+
+const rowForMove = () => {
+  return
+};
+
+const placeMoves = () => {
+  return
+};
